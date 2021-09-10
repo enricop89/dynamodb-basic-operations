04934c1a9f5e5f1736cb2eba17a5ccef9ce072c3
enricop89 <enrico.portolan@gmail.com>
2021-09-10 10:53:57 +0100

    - first commit

--- dynamodb/index.js
+++ dynamodb/index.js
@@ -1,19 +1,63 @@
 const AWS = require( 'aws-sdk' );
-AWS.config.setPromisesDependency( require( 'bluebird' ) );
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const config = require( '../config.json' );
 
-/**
- * Get the customer from Dynamo DB given the Intercom conversationId
- * @param {Intercom Conversation Id} conversationId 
- */
-async function saveItem ( conversationId ) {
-    
-    const getItem = await dynamoDb.get( {
-        TableName: config.DYNAMODB_TABLE,
-        Key: {
-            conversationId: conversationId,
-        },
-    } ).promise();
-    return getItem.Item || null;
-}
+const tableName = config.DYNAMODB_TABLE;
+
+const saveItem = async (item) => {
+    const params = {
+      TableName: tableName,
+      Item: item
+    };
+  
+    await dynamoDb.put(params).promise();
+    return item.itemId;
+  };
+  
+const getItem = async (itemId) => {
+    const params = {
+      Key: {
+        itemId: itemId
+      },
+      TableName: tableName
+    };
+  
+    const result = await dynamoDb.get(params).promise()
+    return result.Item
+  };
+  
+  const deleteItem = async (itemId) => {
+    const params = {
+      Key: {
+        itemId: itemId
+      },
+      TableName: tableName
+    };
+  
+    return dynamoDb.delete(params).promise();
+  };
+  
+const updateItem = async (itemId, paramsName, paramsValue) => {
+    const params = {
+      TableName: tableName,
+      Key: {
+        itemId
+      },
+      ConditionExpression: 'attribute_exists(itemId)',
+      UpdateExpression: 'set ' + paramsName + ' = :v',
+      ExpressionAttributeValues: {
+        ':v': paramsValue
+      },
+      ReturnValues: 'ALL_NEW'
+    };
+  
+    const result = await dynamoDb.update(params).promise()
+      return result.Attributes;
+  };
+
+  module.exports = {
+      saveItem,
+      updateItem,
+      getItem,
+      deleteItem
+  }
